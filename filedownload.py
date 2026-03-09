59cb716a4d85d10356448d403c2f451c553e228a
Teodoro <teodoropartida1@gmail.com>
2026-03-08 23:53:11 -0500

    Refactor file streaming logic

--- filedownload.py
+++ filedownload.py
@@ -7,7 +7,6 @@ import io
 import csv
 
 
-# accept only JSON strings with this structure
 class myReq(BaseModel):
     title: str
     filetype: str
@@ -16,7 +15,6 @@ class myReq(BaseModel):
 
 app = FastAPI()
 
-# to test locally, add your port to origins list. Ex: http://localhost:<PORT>
 origins = ['http://localhost:3000']
 
 app.add_middleware(
@@ -27,42 +25,56 @@ app.add_middleware(
     allow_headers=['*']
 )
 
+def create_csv(data):
+    if not data:
+        raise HTTPException(status_code=400, detail="Data cannot be empty")
+
+    headers = data[0].keys()
+    output = io.StringIO()
+    writer = csv.DictWriter(output, fieldnames=headers)
+    writer.writeheader()
+    writer.writerows(data)
+    output.seek(0)
+
+    return output
+
+
+def create_excel(data):
+    output = io.BytesIO()
+
+    writer = pandas.ExcelWriter(output, engine='xlsxwriter')
+    df = pandas.DataFrame(data)
+
+    df.to_excel(writer, index=False)
+    writer.close()
+
+    output.seek(0)
+
+    return output
 
 @app.post('/file-download')
 async def stream_file(req: myReq):
-    try:
-        file_name = req.title.lower()
-        if req.filetype == 'csv':
-            media_type = 'text/csv'
-            file_name += '.csv'
-            headers = req.data[0].keys()
-            # write to string buffer
-            output = io.StringIO()
-            writer = csv.DictWriter(output, fieldnames=headers)
-            writer.writeheader()
-            writer.writerows(req.data)
-            output.seek(0)
 
-        elif req.filetype == 'excel':
-            media_type = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
-            file_name += '.xlsx'
-            output = io.BytesIO()
-            writer = pandas.ExcelWriter(output, engine='xlsxwriter')
-            df = pandas.DataFrame(req.data)
-            df.to_excel(writer, index=False)
-            writer.close()
-            output.seek(0)
+    file_name = req.title.lower()
 
-        else:
-            raise HTTPException(status_code=406, detail='Filetype is unsupported for download')
+    if req.filetype == "csv":
+        output = create_csv(req.data)
+        media_type = "text/csv"
+        file_name += ".csv"
 
-    except:
-        raise HTTPException(status_code=400, detail='Payload must include title, filetype, and data')
+    elif req.filetype == "excel":
+        output = create_excel(req.data)
+        media_type = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+        file_name += ".xlsx"
+
+    else:
+        raise HTTPException(status_code=406, detail="Unsupported filetype")
 
     return StreamingResponse(
         output,
         media_type=media_type,
-        # Add your port to localhost for local testing
-        headers={'Content-Disposition': f'attachment; filename="{file_name}"',
-                 'Access-Control-Expose-Headers': 'Content-Disposition'}
+        headers={
+            "Content-Disposition": f'attachment; filename="{file_name}"',
+            "Access-Control-Expose-Headers": "Content-Disposition"
+        }
     )
